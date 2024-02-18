a1bf75a8470f43663274750f2d7712e4761a5427
misterNour <159021282+misterNour@users.noreply.github.com>
2024-02-18 12:08:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -39,6 +39,24 @@ def inscription():
     return render_template('inscription.html')
 
 
+
+# Replace with your Google Apps Script web app URL
+GOOGLE_SCRIPT_URL2 = "https://script.google.com/macros/s/AKfycbzS6aAizNGfyb2Qgy4SDsBI2-biRTMJxSQxFY_7H27ZFbwvgXgH87cf5YPRnq_DAzNw_w/exec"
+
+@app.route('/chargilywebhook', methods=['POST'])
+def webbhook():
+    data = request.json  # Assuming the incoming data is in JSON format
+    
+    # Send the data to Google Apps Script
+    response = requests.post(GOOGLE_SCRIPT_URL2, json=data)
+    
+    # Handle the response from the Google Apps Script if needed
+    if response.status_code == 200:
+        return "Data sent to Google Apps Script successfully"
+    else:
+        return "Failed to send data to Google Apps Script"
+
+
     
 if __name__ == '__main__':
     app.run(debug=True)
