fbdca0c48d54e6a6ce2071e388ef8aacc2ea6beb
misterNour <159021282+misterNour@users.noreply.github.com>
2024-02-18 12:08:53 +0100

    Update webbhook.py

--- webbhook.py
+++ webbhook.py
@@ -1,26 +1,26 @@
-from flask import Flask, request
-from flask_cors import CORS 
-import requests
+# from flask import Flask, request
+# from flask_cors import CORS 
+# import requests
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-# Use Flask-CORS with default options
-CORS(app)
+# # Use Flask-CORS with default options
+# CORS(app)
 
-# Replace with your Google Apps Script web app URL
-GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzS6aAizNGfyb2Qgy4SDsBI2-biRTMJxSQxFY_7H27ZFbwvgXgH87cf5YPRnq_DAzNw_w/exec"
+# # Replace with your Google Apps Script web app URL
+# GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzS6aAizNGfyb2Qgy4SDsBI2-biRTMJxSQxFY_7H27ZFbwvgXgH87cf5YPRnq_DAzNw_w/exec"
 
-@app.route('/chargilywebhook', methods=['POST'])
-def webbhook():
-    data = request.json  # Assuming the incoming data is in JSON format
+# @app.route('/chargilywebhook', methods=['POST'])
+# def webbhook():
+#     data = request.json  # Assuming the incoming data is in JSON format
     
-    # Send the data to Google Apps Script
-    response = requests.post(GOOGLE_SCRIPT_URL, json=data)
+#     # Send the data to Google Apps Script
+#     response = requests.post(GOOGLE_SCRIPT_URL, json=data)
     
-    # Handle the response from the Google Apps Script if needed
-    if response.status_code == 200:
-        return "Data sent to Google Apps Script successfully"
-    else:
-        return "Failed to send data to Google Apps Script"
+#     # Handle the response from the Google Apps Script if needed
+#     if response.status_code == 200:
+#         return "Data sent to Google Apps Script successfully"
+#     else:
+#         return "Failed to send data to Google Apps Script"
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
